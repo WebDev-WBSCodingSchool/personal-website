0fd2d765b539edeeb35288837766d086bcba7aad
Jorge Paul <jserranopaul@gmail.com>
2025-03-25 17:16:07 +0100

    Admin layout

--- personal_website/routes/admin.py
+++ personal_website/routes/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from ..routes.auth import login_required
 from ..db import get_connection
 
@@ -8,3 +8,15 @@ admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 @login_required
 def admin():
    return render_template('admin/admin.html')
+
+@admin_bp.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+   if request.method == 'POST':
+      print("Updating profile")  
+   return render_template('admin/edit-profile.html')
+
+@admin_bp.route('/edit-projects')
+@login_required
+def edit_projects():
+   return render_template('admin/edit-projects.html')
